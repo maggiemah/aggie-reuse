a46cf0d54704485e1e7340c3a8c366ce969bd958
jiff3 <42486234+jiff3@users.noreply.github.com>
2023-05-21 00:55:10 -0700

    server.js updates

--- backend/server.js
+++ backend/server.js
@@ -24,22 +24,6 @@ const itemSchema = new mongoose.Schema({
   supplier: String
 });
 
-function findPreviousDay(dateString) {
-  // Parse the date
-  let dateParts = dateString.split('/');
-  let dateObject = new Date(+dateParts[2], dateParts[0] - 1, +dateParts[1]);
-
-  // Subtract one day
-  dateObject.setDate(dateObject.getDate() - 1);
-
-  // Format the date in 'MM/DD/YY' format
-  let year = dateObject.getFullYear().toString().substr(2, 2);
-  let month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
-  let day = dateObject.getDate().toString().padStart(2, '0');
-
-  return `${month}/${day}/${year}`;
-}
-
 // Returns JSON of document of requested collection
 app.get('/getitems/:collectionName', async (req, res) => {
   const name = req.params.collectionName;
@@ -48,29 +32,9 @@ app.get('/getitems/:collectionName', async (req, res) => {
   const collections = await mongoose.connection.db.listCollections().toArray();
   const collectionNames = collections.map(c => c.name);
 
-  // If the collection requested doesn't exist, then create a new one only if the collection requested is a date not in the future
+  // If the collection requested doesn't exist, then return error
   if (!collectionNames.includes(name)) {
-    return res.status(404).send('Collectiom not found');
-    /*try {
-      const Item = mongoose.model(name, itemSchema, name);
-      const items = await Item.find({ }); // Creates collection with name var as its name
-
-      // Create new documents in 'Items'
-      const copiedItems = items.map(item => {
-        // Remove '_id' property to avoid duplicate key error
-        const { ...rest } = item._doc;
-        return rest;
-      });
-
-      // Insert copied items into the 'Items' collection
-      await Item.insertMany(copiedItems);
-
-      res.status(200).send('Items collection has been duplicated into Items2');
-
-    } catch (err) {
-      console.log(err);
-      res.status(500).send(err.message);
-    }*/
+    return res.status(404).send('Collection not found');
   }
 
   const Item = mongoose.model(name, itemSchema, name);
@@ -84,11 +48,45 @@ app.get('/getitems/:collectionName', async (req, res) => {
 });
 
 // Route to update item quantity by its name
-app.put('/updateitem/:collectionName/:name/:increment', async (req, res) => {
+app.get('/updateitem/:collectionName/:name/:increment', async (req, res) => {
   const collectionName = req.params.collectionName;
-  const Item = mongoose.model(name, itemSchema, name);
-  const { name } = req.params.name; // name stored in document
-  const { increment } = req.params.increment; // 0 means decrement by 1, 1 means increment by 1
+
+  // Gets all collections to check if requested collection exists
+  const collections = await mongoose.connection.db.listCollections().toArray();
+  const collectionNames = collections.map(c => c.name);
+
+  // If the collection requested doesn't exist, then create a new one
+  if (!collectionNames.includes(collectionName)) {
+    // Initialize new collection with 20 new documents for each item
+    const Item = mongoose.model(collectionName, itemSchema, collectionName);
+    const newItems = [
+      { id: 01, name: "Bag/Backpack", quantity: 0, price: 0, supplier: "None" },
+      { id: 02, name: "Belt", quantity: 0, price: 0, supplier: "None" },
+      { id: 03, name: "Books", quantity: 0, price: 0, supplier: "None" },
+      { id: 04, name: "Dress", quantity: 0, price: 0, supplier: "None" },
+      { id: 05, name: "Hat", quantity: 0, price: 0, supplier: "None" },
+      { id: 06, name: "Household", quantity: 0, price: 0, supplier: "None" },
+      { id: 07, name: "Jacket", quantity: 0, price: 0, supplier: "None" },
+      { id: 08, name: "Long-sleeve/button-up", quantity: 0, price: 0, supplier: "None" },
+      { id: 09, name: "Pants/Jeans", quantity: 0, price: 0, supplier: "None" },
+      { id: 10, name: "Ring/Jewelry", quantity: 0, price: 0, supplier: "None" },
+      { id: 11, name: "School_supplies", quantity: 0, price: 0, supplier: "None" },
+      { id: 12, name: "Shirts", quantity: 0, price: 0, supplier: "None" },
+      { id: 13, name: "Shoes", quantity: 0, price: 0, supplier: "None" },
+      { id: 14, name: "Shorts", quantity: 0, price: 0, supplier: "None" },
+      { id: 15, name: "Skirt", quantity: 0, price: 0, supplier: "None" },
+      { id: 16, name: "Sunglasses", quantity: 0, price: 0, supplier: "None" },
+      { id: 17, name: "Sweater/Cardigan", quantity: 0, price: 0, supplier: "None" },
+      { id: 18, name: "Tank_top", quantity: 0, price: 0, supplier: "None" },
+      { id: 19, name: "Tie", quantity: 0, price: 0, supplier: "None" },
+      { id: 20, name: "Miscellaneous", quantity: 0, price: 0, supplier: "None" }
+      ];
+      await Item.insertMany(newItems);
+  }
+
+  const Item = mongoose.model(collectionName, itemSchema, collectionName);
+  const name = req.params.name; // name stored in document
+  let increment = Number(req.params.increment); // 0 means decrement by 1, 1 means increment by 1
   if(increment == 0){increment = -1;}
 
   try {
@@ -109,7 +107,6 @@ app.put('/updateitem/:collectionName/:name/:increment', async (req, res) => {
   }
 });
 
-
 app.get("/", (req, res) => {
   res.send("connected");
 });
@@ -118,3 +115,35 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Server listening the port http://localhost/" + port);
 });
+
+
+
+
+
+
+
+
+
+
+// Unused code for duplicating specified collection
+
+    /*try {
+      const Item = mongoose.model(name, itemSchema, name);
+      const items = await Item.find({ }); // Creates collection with name var as its name
+
+      // Create new documents in 'Items'
+      const copiedItems = items.map(item => {
+        // Remove '_id' property to avoid duplicate key error
+        const { ...rest } = item._doc;
+        return rest;
+      });
+
+      // Insert copied items into the 'Items' collection
+      await Item.insertMany(copiedItems);
+
+      res.status(200).send('Items collection has been duplicated into Items2');
+
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err.message);
+    }*/
